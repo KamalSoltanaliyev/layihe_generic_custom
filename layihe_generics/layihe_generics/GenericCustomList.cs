c03ea65c0d2319c540ec1313a145f0c5fcb53ee9
KamalSoltanaliyev <148267364+KamalSoltanaliyev@users.noreply.github.com>
2023-11-30 17:04:39 +0400

    Add files via upload

---
+++ layihe_generics/layihe_generics/GenericCustomList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace layihe_generics
+{
+    class GenericCustomList<T>
+    {
+        private T[] items;
+        private int count;
+        private int capacity;
+
+        public GenericCustomList()
+        {
+            items = new T[0];
+            count = 0;
+            capacity = 4;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+
+        public void Add(T item)
+        {
+            if (count == capacity)
+            {
+                capacity *= 2;
+                Array.Resize(ref items, capacity);
+            }
+            items[count] = item;
+            count++;
+        }
+
+        public T Find(Predicate<T> match)
+        {
+            return Array.Find(items, match);
+        }
+
+        public GenericCustomList<T> FindAll(Predicate<T> match)
+        {
+            Array.FindAll<T>(items, match);
+
+            return this;
+        }
+
+
+        public bool Contains(T item)
+        {
+            return Array.IndexOf(items, item) != -1;
+        }
+
+        public bool Exists(Predicate<T> match)
+        {
+            return Array.Exists(items, match);
+        }
+
+        public void Remove(T item)
+        {
+            int index = Array.IndexOf(items, item);
+            if (index != -1)
+            {
+                for (int i = index; i < count - 1; i++)
+                {
+                    items[i] = items[i + 1];
+                }
+                count--;
+            }
+        }
+    }
+}
---
+++ layihe_generics/layihe_generics/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        GenericCustomList<int> customList = new GenericCustomList<int>();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            customList.Add(i);
+        }
+
+        Console.WriteLine($"Count: {customList.Count}");
+        Console.WriteLine($"Capacity: {customList.Capacity}");
+
+        // Find methodu
+        int foundItem = customList.Find(x => x > 7);
+        Console.WriteLine("Find: " + foundItem);
+        
+        // FindAll methodu
+        List<int> foundItems = customList.FindAll(x => x % 2 == 0);
+        Console.Write("FindAll even numbers: ");
+        foreach (int item in foundItems)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+
+        // Contains methodu
+        bool containsItem = customList.Contains(5);
+        Console.WriteLine("Contains item 5: " + containsItem);
+
+        // Exists methodu
+        bool existsItem = customList.Exists(x => x == 14);
+        Console.WriteLine("Exists item 14: " + existsItem);
+
+        // Remove methodu
+        customList.Remove(3);
+        Console.Write("After removing 3: ");
+        foreach (int item in customList)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+class GenericCustomList<T> : List<T>
+{
+    
+}
